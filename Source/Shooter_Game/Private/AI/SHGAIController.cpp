ef650a02e4da16d62aed663106dd1443e356c883
S1yGus <Holy-Fox@mail.ru>
2023-02-27 00:05:18 +0300

    AI controller has been refactored

--- Source/Shooter_Game/Private/AI/SHGAIController.cpp
+++ Source/Shooter_Game/Private/AI/SHGAIController.cpp
@@ -5,7 +5,10 @@
 #include "Components/ShooterAIPerceptionComponent.h"
 #include "Components/SHGRespawnComponent.h"
 #include "BehaviorTree/BlackboardComponent.h"
+#include "BehaviorTree/Blackboard/BlackboardKeyType_Object.h"
 #include "Perception/AISense_Prediction.h"
+
+constexpr static float SensePredictionUpdateTimerRate = 0.1f;
 
 ASHGAIController::ASHGAIController()
 {
@@ -18,22 +21,15 @@ ASHGAIController::ASHGAIController()
     bAllowStrafe = true;
 }
 
-void ASHGAIController::Tick(float DeltaTime)
-{
-    Super::Tick(DeltaTime);
-
-    const auto FocusOnActor = GetFocusOnActor();
-    SetFocus(FocusOnActor);
-
-    UAISense_Prediction::RequestControllerPredictionEvent(this, FocusOnActor, PredictionTime);
-}
-
 void ASHGAIController::BeginPlay()
 {
     Super::BeginPlay();
 
     check(AIPerceptionComponent);
     check(RespawnComponent);
+
+    FTimerHandle SensePredictionUpdateTimerHandle;
+    GetWorldTimerManager().SetTimer(SensePredictionUpdateTimerHandle, this, &ThisClass::OnSensePredictionUpdate, SensePredictionUpdateTimerRate, true);
 }
 
 void ASHGAIController::OnPossess(APawn* InPawn)
@@ -42,27 +38,36 @@ void ASHGAIController::OnPossess(APawn* InPawn)
 
     if (const auto AICharacter = Cast<ASHGAICharacter>(InPawn))
     {
+        RunBehaviorTree(AICharacter->GetBehaviorTree());
+
         AICharacter->OnTakeAnyDamage.AddDynamic(this, &ThisClass::OnTakeDamage);
 
-        RunBehaviorTree(AICharacter->GetBehaviorTree());
+        if (const auto BlackboardComponent = GetBlackboardComponent())
+        {
+            OnEnemyKeyChanged.BindLambda(
+                [&](const UBlackboardComponent& BlackboardComponent, FBlackboard::FKey KeyID)
+                {
+                    FocusOnActor = Cast<AActor>(BlackboardComponent.GetValue<UBlackboardKeyType_Object>(KeyID));
+                    SetFocus(FocusOnActor);
+
+                    return EBlackboardNotificationResult::ContinueObserving;
+                });
+
+            BlackboardComponent->RegisterObserver(BlackboardComponent->GetKeyID(EnemyBlackboardKeyName), this, OnEnemyKeyChanged);
+        }
     }
 }
 
 void ASHGAIController::OnTakeDamage(AActor* DamagedActor, float Damage, const UDamageType* DamageType, AController* InstigatedBy, AActor* DamageCauser)
 {
     const auto BlackboardComponent = GetBlackboardComponent();
-    if (!BlackboardComponent || GetFocusOnActor() || !InstigatedBy)
+    if (!BlackboardComponent || BlackboardComponent->GetValueAsObject(EnemyBlackboardKeyName) || !InstigatedBy)
         return;
 
     BlackboardComponent->SetValueAsObject(EnemyBlackboardKeyName, InstigatedBy->GetPawn());
 }
 
-AActor* ASHGAIController::GetFocusOnActor()
+void ASHGAIController::OnSensePredictionUpdate()
 {
-    if (const auto BlackboardComponent = GetBlackboardComponent())
-    {
-        return Cast<AActor>(BlackboardComponent->GetValueAsObject(EnemyBlackboardKeyName));
-    }
-
-    return nullptr;
+    UAISense_Prediction::RequestControllerPredictionEvent(this, FocusOnActor, PredictionTime);
 }
--- Source/Shooter_Game/Public/AI/SHGAIController.h
+++ Source/Shooter_Game/Public/AI/SHGAIController.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "AIController.h"
+#include "BehaviorTree/BehaviorTreeTypes.h"
 #include "SHGAIController.generated.h"
 
 class USHGAIPerceptionComponent;
@@ -17,27 +18,29 @@ class SHOOTER_GAME_API ASHGAIController : public AAIController
 public:
     ASHGAIController();
 
-    virtual void Tick(float DeltaTime) override;
-
 protected:
-    UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "Components")
+    UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Components")
     USHGAIPerceptionComponent* AIPerceptionComponent;
 
-    UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "Components")
+    UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Components")
     USHGRespawnComponent* RespawnComponent;
 
     UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "AI")
-    FName EnemyBlackboardKeyName = "Enemy";
+    FName EnemyBlackboardKeyName{"Enemy"};
 
     UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "AI")
-    float PredictionTime = 0.2f;
+    float PredictionTime = 0.3f;
 
     virtual void BeginPlay() override;
     virtual void OnPossess(APawn* InPawn) override;
 
 private:
-    AActor* GetFocusOnActor();
+    FOnBlackboardChangeNotification OnEnemyKeyChanged;
+
+    UPROPERTY()
+    AActor* FocusOnActor;
 
     UFUNCTION()
     void OnTakeDamage(AActor* DamagedActor, float Damage, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser);
+    void OnSensePredictionUpdate();
 };
