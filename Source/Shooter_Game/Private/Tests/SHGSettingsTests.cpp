98cb07bee86296123b9b9602f491136964f59569
S1yGus <Holy-Fox@mail.ru>
2023-05-11 22:35:26 +0300

    game user settings test has been added

---
+++ Source/Shooter_Game/Private/Tests/SHGSettingsTests.cpp
@@ -0,0 +1,23 @@
+// Shooter_Game, All rights reserved.
+
+#if WITH_AUTOMATION_TESTS
+
+#include "Tests/SHGPlayerControllerTests.h"
+#include "CoreMinimal.h"
+#include "Tests/Utils/SHGTestsUtils.h"
+#include "Misc/AutomationTest.h"
+#include "Settings/SHGGameUserSettings.h"
+
+using namespace Tests;
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FGameUserSettingsShouldExists, "Shooter_Game.Settings.GameUserSettingsShouldExists",
+                                 EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter | EAutomationTestFlags::HighPriority);
+
+bool FGameUserSettingsShouldExists::RunTest(const FString& Parameters)
+{
+    TestNotNull("SHGGameUserSettings should exists", USHGGameUserSettings::Get());
+
+    return true;
+}
+
+#endif    // WITH_AUTOMATION_TESTS
