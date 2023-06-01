c7f85c6adea282732fc7d10b46487da268680849
S1yGus <Holy-Fox@mail.ru>
2023-06-02 01:14:51 +0300

    error c2374 has been really fixed

--- Source/Shooter_Game.Target.cs
+++ Source/Shooter_Game.Target.cs
@@ -5,11 +5,12 @@ using System.Collections.Generic;
 
 public class Shooter_GameTarget : TargetRules
 {
-	public Shooter_GameTarget(TargetInfo Target) : base(Target)
-	{
-		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+    public Shooter_GameTarget(TargetInfo Target) : base(Target)
+    {
+        Type = TargetType.Game;
+        DefaultBuildSettings = BuildSettingsVersion.V2;
+        bUseUnityBuild = false;
 
-		ExtraModuleNames.AddRange( new string[] { "Shooter_Game" } );
-	}
+        ExtraModuleNames.AddRange(new string[] { "Shooter_Game" });
+    }
 }
